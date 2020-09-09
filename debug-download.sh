9d168f9c5e3f4ff216d24c383dbc49d0b0a39e2e
mitchharpur <mitchharpur@me.com>
2020-09-09 19:24:34 -0400

    update script

--- debug-download.sh
+++ debug-download.sh
@@ -2,9 +2,14 @@ repo="https://raw.githubusercontent.com/mitchharpur/openshift-console-debugging/
 
 for shellScript in debug-{attach,build,connect,environment,get-ca-certificate,get-oauth-secret,launch,run}.sh
 do
+    if [ -f ./$shellScript ]
+    then
+        echo "Removing : $shellScript ..."
+        rm ./$shellScript
+    fi
     url="$repo$shellScript"
     echo "Downloading : $url ..."
     curl -LJO $url
-    echo "Setting permissions : $shellScript ..."
+    echo "Setting execute permissions : $shellScript ..."
     chmod u+x $shellScript
 done

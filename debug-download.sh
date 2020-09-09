235b69df0ff0266893ac2cc62c25708683496be9
mitchharpur <mitchharpur@me.com>
2020-09-09 18:53:40 -0400

    added some download and init scripts

---
+++ debug-download.sh
@@ -0,0 +1,10 @@
+repo="https://github.com/mitchharpur/openshift-console-debugging/blob/master/"
+
+for shellScript in debug-{attach,build,connect,environment,get-ca-certificate,get-oauth-secret,launch,run}.sh
+do
+    url="$repo$shellScript"
+    echo "Downloading : $url ..."
+    curl -LJO $url
+    echo "Setting permissions : $shellScript ..."
+    chmod u+x $shellScript
+done

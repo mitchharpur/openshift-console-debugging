33d2fb552813840c3a7fd089f0ddb7ad413d277b
mitchharpur <mitchharpur@me.com>
2020-09-10 23:24:31 -0400

    added vscode downloads

--- debug-download.sh
+++ debug-download.sh
@@ -1,5 +1,18 @@
 repo="https://raw.githubusercontent.com/mitchharpur/openshift-console-debugging/master/"
 
+function downloadVSCodeFile(){
+  local file=".vscode/$1"
+  if [ -f file  ]
+  then
+    #make a copy of the old file
+    mv file basename $(mktemp -u -t $file)
+  fi
+  local url="$repo$file"
+  #download the new file
+  echo "Downloading : $url ..."
+  curl -LJO $url
+}
+
 for shellScript in debug-{attach,build,connect,environment,get-ca-certificate,get-oauth-secret,launch,run}.sh
 do
     if [ -f ./$shellScript ]
@@ -13,3 +26,10 @@ do
     echo "Setting execute permissions : $shellScript ..."
     chmod u+x $shellScript
 done
+# create the folder if it does not exist
+[[ -d ./.vscode ]] || mkdir ./.vscode
+for file in {launch,tasks}.json
+do
+ downloadVSCodeFile $file
+done
+unset file

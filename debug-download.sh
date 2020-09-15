fbbc18da0437d46f7ae9ba2bfd23c499e6178635
mitchharpur <mitchharpur@me.com>
2020-09-15 19:47:45 -0400

    update download

--- debug-download.sh
+++ debug-download.sh
@@ -1,16 +1,16 @@
-reset=`tput sgr0`
-bold=`tput bold`
+local reset=$(tput sgr0);
+local bold=$(tput bold);
 
-red="$(tput setaf 1)"                      # bright red text
-green=$(tput setaf 2) 
-darkcyan=$(tput setaf 6)                  # dim cyan text
-cyan="$bold$darkcyan"                     # bright cyan text
-gray=$(tput setaf 7)                      # dim white text
-darkgray="$bold"$(tput setaf 0)           # bold black = dark gray text
-white="$bold$gray"                        # bright white text
-yellow=$(tput setaf 3)                   # dark yellow text
+local red="$(tput setaf 1)";                      # bright red text
+local green=$(tput setaf 2); 
+local darkcyan=$(tput setaf 6);                  # dim cyan text
+local cyan="$bold$darkcyan";                     # bright cyan text
+local gray=$(tput setaf 7);                      # dim white text
+local darkgray="$bold"$(tput setaf 0);           # bold black = dark gray text
+local white="$bold$gray";                        # bright white text
+local yellow=$(tput setaf 3);                   # dark yellow text
 
-repo="https://raw.githubusercontent.com/mitchharpur/openshift-console-debugging/master/"
+local repo="https://raw.githubusercontent.com/mitchharpur/openshift-console-debugging/master/"
 
 
 

b82dbbb2b1d8cb667a1a33c4a94d15fe5669a16c
mitchharpur <mitchharpur@me.com>
2020-09-15 19:33:18 -0400

    updating download

--- debug-download.sh
+++ debug-download.sh
@@ -1,25 +1,49 @@
+reset=`tput sgr0`
+bold=`tput bold`
+
+red="$(tput setaf 1)"                      # bright red text
+green=$(tput setaf 2) 
+darkcyan=$(tput setaf 6)                  # dim cyan text
+cyan="$bold$darkcyan"                     # bright cyan text
+gray=$(tput setaf 7)                      # dim white text
+darkgray="$bold"$(tput setaf 0)           # bold black = dark gray text
+white="$bold$gray"                        # bright white text
+yellow=$(tput setaf 3);                   # dark yellow text
+
 repo="https://raw.githubusercontent.com/mitchharpur/openshift-console-debugging/master/"
 
+
+
 function downloadVSCodeFile(){
-  local file=.vscode/$1
-  if [ -f $file  ]
+  local fileName="$1"
+  local fileBaseName="${fileName%.*}";
+  local fileExtension="${fileName##*.}";
+  local fileFolder=".vscode/";
+  local filePath="./$fileFolder$fileName";
+  
+  if [[ -f $filePath  ]]
   then
-    #make a copy of the old file
-    mv $file .vscode/$(basename $(mktemp -u $file.XXXX))
+    #make a copy of the old file in order not to overwrite any previous developer settings
+    local backupName=$(mktemp -u $fileBaseName.old.XXXX.$fileExtension)
+    echo "$red$bold Note: $fileName already exists .... making a backup of the existing $fileName into $backupName $reset"
+    mv $filePath $fileFolder$backupName
   fi
-  local url="$repo$file"
-  #download the new file
-  curl -fSL $url -o $file
+  local url="$repo$fileFolder$fileName"
+  #download the file file
+  echo "$cyan Downloading $filePath $reset"
+  curl -fSL $url -o $filePath
 }
 
 for shellScript in debug-{attach,build,connect,environment,get-ca-certificate,get-oauth-secret,launch,run,install-plugins}.sh
 do
-    if [ -f ./$shellScript ]
+    if [[ -f ./$shellScript ]]
     then
       rm ./$shellScript
     fi
     url="$repo$shellScript"
+    echo -e "\n$cyan Downloading $shellScript $reset"
     curl -LJO $url
+    echo "$yellow$bold Setting $shellScript mode to be executable $reset"
     chmod u+x $shellScript
 done
 # create the folder if it does not exist

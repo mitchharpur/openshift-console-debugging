92d52b5723f66ae697162a46c82ebfaf8e8f8099
mitchharpur <mitchharpur@me.com>
2020-09-15 23:11:33 -0400

    update

--- debug-download.sh
+++ debug-download.sh
@@ -1,16 +1,16 @@
 # set -x
 #function setColors(){
-reset=$(tput sgr0);
-bold=$(tput bold);
+reset=$(tput sgr0)
+bold=$(tput bold)
 
-red=$(tput setaf 1);                      # bright red text
-green=$(tput setaf 2);
-darkcyan=$(tput setaf 6);                  # dim cyan text
-cyan=$bold$darkcyan;                     # bright cyan text
-gray=$(tput setaf 7);                      # dim white text
-darkgray=$bold$(tput setaf 0);           # bold black = dark gray text
-white=$bold$gray;                        # bright white text
-yellow=$(tput setaf 3);                   # dark yellow text
+red=$(tput setaf 1)                      # bright red text
+green=$(tput setaf 2)
+darkcyan=$(tput setaf 6)                  # dim cyan text
+cyan=$bold$darkcyan                     # bright cyan text
+gray=$(tput setaf 7)                      # dim white text
+darkgray=$bold$(tput setaf 0)           # bold black = dark gray text
+white=$bold$gray                        # bright white text
+yellow=$(tput setaf 3)                   # dark yellow text
 
 repo="https://raw.githubusercontent.com/mitchharpur/openshift-console-debugging/master/"
 #}

a87de1231020d2efaecc2a564fbf67cf94dbc9b6
mitchharpur <mitchharpur@me.com>
2020-09-15 21:54:01 -0400

    update

--- debug-download.sh
+++ debug-download.sh
@@ -26,7 +26,7 @@ function downloadVSCodeFile(){
   then
     #make a copy of the old file in order not to overwrite any previous developer settings
     local backupName=$(mktemp -u $fileBaseName.old.XXXX.$fileExtension)
-    echo "Note1 : Note2"
+    # echo "Note1 : Note2"
     # echo -e "Note: $fileName already exists .... making a backup of the existing $fileName into $backupName"
     # echo -e $("$red$bold Note: $fileName already exists .... making a backup of the existing $fileName into $backupName $reset")
     mv $filePath $fileFolder$backupName

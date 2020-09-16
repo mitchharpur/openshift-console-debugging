69e5ff70fd71754140a6e33f8faafc55dc16fc0b
mitchharpur <mitchharpur@me.com>
2020-09-15 23:02:00 -0400

    update

--- debug-download.sh
+++ debug-download.sh
@@ -1,16 +1,16 @@
 # set -x
 #function setColors(){
-reset="";#$(tput sgr0);
-bold="";#$(tput bold);
+reset=$(tput sgr0);
+bold=$(tput bold);
 
-red="";#"$(tput setaf 1)";                      # bright red text
-green="";#$(tput setaf 2);
-darkcyan="";#$(tput setaf 6);                  # dim cyan text
-cyan="";#"$bold$darkcyan";                     # bright cyan text
-gray="";#$(tput setaf 7);                      # dim white text
-darkgray="";#"$bold"$(tput setaf 0);           # bold black = dark gray text
-white="";#"$bold$gray";                        # bright white text
-yellow="";#$(tput setaf 3);                   # dark yellow text
+red=$(tput setaf 1);                      # bright red text
+green=$(tput setaf 2);
+darkcyan=$(tput setaf 6);                  # dim cyan text
+cyan=$bold$darkcyan;                     # bright cyan text
+gray=$(tput setaf 7);                      # dim white text
+darkgray=$bold$(tput setaf 0);           # bold black = dark gray text
+white=$bold$gray;                        # bright white text
+yellow=$(tput setaf 3);                   # dark yellow text
 
 repo="https://raw.githubusercontent.com/mitchharpur/openshift-console-debugging/master/"
 #}
@@ -36,8 +36,8 @@ function downloadVSCodeFile(){
     fi
     if [[ $backups -eq 0 ]]
     then
-      echo "Note: $fileName already exists .... making a backup of the existing $fileName into $backupName"
-      # echo -e $("$red$bold Note: $fileName already exists .... making a backup of the existing $fileName into $backupName $reset")
+      #echo "Note: $fileName already exists .... making a backup of the existing $fileName into $backupName"
+      echo -e $("$red$bold Note: $fileName already exists .... making a backup of the existing $fileName into $backupName $reset")
       mv $filePath $fileFolder$backupName
     fi
   fi

80098e525220b56c81b73d3a6435c57dd408a6b3
mitchharpur <mitchharpur@me.com>
2020-09-16 16:02:49 -0400

    add go and delve version

--- debug-environment.sh
+++ debug-environment.sh
@@ -20,7 +20,7 @@ checkClusterStatus(){
 
   # all Bourne-style shells support file descriptor reassignment i.e 2>&1
   # note: oc std error stream is redirected to std out stream in order to gep it
-  declare fullStatus=$(oc status 2>&1)
+  declare fullStatus=$(oc status 2>&1 > /dev/null)
   declare status=${fullStatus%%$'\n'*}  #$($fullstatus | head -n 1 )
   if [[ $status == *"$connectError"* ]]; then
     echo -e "\n${cyan} - Status             : \n\n${red}${bold}$status${reset}"
@@ -38,7 +38,7 @@ checkClusterStatus(){
   echo -e "\n${cyan} - Status             : ${green}${bold}Cluster Available : ${reset}${status}"
   return 0
 }
-checkClusterStatus
+#checkClusterStatus
 #configure environment variables and write them to a .env file
 configureDebugEnvironment(){
   # all Bourne-style shells support file descriptor reassignment i.e 2>&1
@@ -76,15 +76,35 @@ configureDebugEnvironment(){
         export CONSOLE_CLUSTER_URL=$(oc whoami --show-server)
       fi
   fi
-  #other misc env variables
+  # go version
+  declare goVersion=$(go version | cut -d' ' -f3 )
+  declare GOVERSION="Missing or incorrect go installation ..."
+  if [[ $goVersion == "go"* ]]
+  then
+    GOVERSION=${goVersion#go}
+  fi
+  declare dlvVersion=$(dlv version | sed -n '2 p' 2>&1 )
+  declare DELVEVERSION="Missing or incorrect delve debugger installation ..."
+
+  # delve debugger version"
+  if [[ $dlvVersion == "Version: "* ]]
+  then
+    DELVEVERSION=$(echo -e $dlvVersion|cut -d' ' -f2)
+  fi
+
   export CONSOLE_DEBUGGER_API_PORT=2345
   export CONSOLE_HOST_NAME=0.0.0.0 # $HOSTNAME # using the host name still has some problems with server origin testing in backend ..i.e web can only be 'localhost'
   export CONSOLE_API_PORT=9000
 
-  GIT_TAG=${SOURCE_GIT_TAG:-$(git describe --always --tags HEAD)}
+  declare gitStatus=$(git status 2>&1 > /dev/null)
+  if [[ $gitStatus == *"not a git repository"* ]]
+  then
+    GIT_TAG="0.0.0-no-version-found"
+  else
+    GIT_TAG=${SOURCE_GIT_TAG:-$(git describe --always --tags HEAD)}
+  fi
   LD_FLAGS="'-X github.com/openshift/console/pkg/version.Version=${GIT_TAG}'"
   GC_FLAGS="'all=-N -l'"
-
 
   export CONSOLE_BUILD_FLAGS="-gcflags=${GC_FLAGS} -ldflags=${LD_FLAGS}"
   #create the environment file contents
@@ -98,14 +118,16 @@ configureDebugEnvironment(){
   environmentFileContents=$environmentFileContents'\nCONSOLE_DEBUGGER_API_PORT='$CONSOLE_DEBUGGER_API_PORT
   environmentFileContents=$environmentFileContents'\nCONSOLE_BUILD_FLAGS='$CONSOLE_BUILD_FLAGS
   #pipe environment file contents to the debug-environement file
-  echo -e "\n${bold}${yellow} - Environment File   :${white} ${environmentFileName}"
-  echo -e "${yellow} - Debugger Api Port  : ${white}${CONSOLE_DEBUGGER_API_PORT}"
-  echo -e "${yellow} - Debugger Host      : ${white}${CONSOLE_HOST_NAME}"
-  echo -e "${cyan} - Console Version    : ${white}${GIT_TAG}"
-  echo -e "${cyan} - Console Api Port   : ${white}${CONSOLE_API_PORT}"
-  echo -e   "${cyan} - Cluster Url        : ${white}$CONSOLE_CLUSTER_URL"
-  echo -e   "${cyan} - AlertManager Url   : ${white}$CONSOLE_ALERTMANAGER_URL"
-  echo -e   "${cyan} - Thanos Url         : ${white}$CONSOLE_THANOS_URL"
+  echo -e "\n${bold}${yellow} - Environment File        :${white} ${environmentFileName}"
+  echo -e "${yellow} - Go Version              : ${white}${GOVERSION}"
+  echo -e "${yellow} - Delve Debugger Version  : ${white}${DELVEVERSION}"
+  echo -e "${yellow} - Delve Debugger Api Port : ${white}${CONSOLE_DEBUGGER_API_PORT}"
+  echo -e "${yellow} - Delve Debugger Host     : ${white}${CONSOLE_HOST_NAME}"
+  echo -e "${cyan} - Console Version         : ${white}${GIT_TAG}"
+  echo -e "${cyan} - Console Api Port        : ${white}${CONSOLE_API_PORT}"
+  echo -e   "${cyan} - Cluster Url             : ${white}$CONSOLE_CLUSTER_URL"
+  echo -e   "${cyan} - AlertManager Url        : ${white}$CONSOLE_ALERTMANAGER_URL"
+  echo -e   "${cyan} - Thanos Url              : ${white}$CONSOLE_THANOS_URL"
   touch ./$environmentFileName
   echo -e $environmentFileContents > ./$environmentFileName
 
@@ -113,4 +135,4 @@ configureDebugEnvironment(){
 
 }
 
-#configureDebugEnvironment
+configureDebugEnvironment

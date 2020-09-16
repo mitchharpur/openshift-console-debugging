0b40aa14ed375a47b40a837409b9c329c4b52b2a
mitchharpur <mitchharpur@me.com>
2020-09-16 16:36:03 -0400

    add new line

--- debug-download.sh
+++ debug-download.sh
@@ -11,7 +11,7 @@ darkgray=$bold$(tput setaf 0)           # bold black = dark gray text
 white=$bold$gray                        # bright white text
 yellow=$(tput setaf 3)                  # dark yellow text
 
-echo -e "$green$bold *** Downloading debug-*.sh files ... *** $reset"
+echo -e "\n$green$bold *** Downloading debug-*.sh files ... *** $reset\n"
 
 
 repo="https://raw.githubusercontent.com/mitchharpur/openshift-console-debugging/master/"
